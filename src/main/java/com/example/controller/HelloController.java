d3f43381f8e97c13f2f0c2307275c45c8a0efe15
Mounika Manchala <mounikamanchala25@gmail.com>
2023-05-19 12:09:51 +0530

    first commit

---
+++ src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/")
+	public String showIndex() {
+		return "index";
+	}
+
+	@RequestMapping(method = RequestMethod.POST, path = "/hello")
+	public String sayHello(HttpServletRequest request) {
+		request.setAttribute("ATTR1", request.getParameter("t1"));
+		return "hello";
+	}
+
+}
---
+++ target/mvc-demo2/WEB-INF/view/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Spring MVC project 2</title>
+</head>
+<body>
+	<form action="hello" method="post">
+		<input type="text" name="t1"><br /> <input type="submit"
+			value="sayHello">
+
+	</form>
+</body>
+</html>
